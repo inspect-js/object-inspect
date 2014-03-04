e324033267025995ec97d32ed0a65737c99477a6
James Halliday <mail@substack.net>
2014-03-04 15:27:44 -0800

    regex already work

---
+++ example/all.js
@@ -0,0 +1,10 @@
+var inspect = require('../');
+var obj = {
+    a: 1,
+    b: [ 3, 4, undefined, null ],
+    c: undefined,
+    d: null,
+    e: /^x/i
+};
+obj.self = obj;
+console.log(inspect(obj));
--- example/undef.js
+++
@@ -1,4 +0,0 @@
-var inspect = require('../');
-var obj = { a: 1, b: [ 3, 4, undefined, null ], c: undefined, d: null };
-obj.c = obj;
-console.log(inspect(obj));
