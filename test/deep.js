e12b91cd59683362f3a0e80f46481a0211e26c15
James Halliday <mail@substack.net>
2014-08-05 13:09:45 +1200

    coverage for explicit depth param

--- test/deep.js
+++ test/deep.js
@@ -2,7 +2,8 @@ var inspect = require('../');
 var test = require('tape');
 
 test('deep', function (t) {
-    t.plan(1);
+    t.plan(2);
     var obj = [ [ [ [ [ [ 500 ] ] ] ] ] ];
     t.equal(inspect(obj), '[ [ [ [ [ [Object] ] ] ] ] ]');
+    t.equal(inspect(obj, { depth: 2 }), '[ [ [Object] ] ]');
 });
