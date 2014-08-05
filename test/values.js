8d753fb362a534fa1106e4d80f2ee9bea06a66d9
James Halliday <mail@substack.net>
2014-08-05 13:36:17 +1200

    exercise hasOwnProperty path

--- test/values.js
+++ test/values.js
@@ -6,3 +6,11 @@ test('values', function (t) {
     var obj = [ {}, [], { 'a-b': 5 } ];
     t.equal(inspect(obj), '[ {}, [], { \'a-b\': 5 } ]');
 });
+
+test('has', function (t) {
+    t.plan(1);
+    var has = Object.prototype.hasOwnProperty;
+    delete Object.prototype.hasOwnProperty;
+    t.equal(inspect({ a: 1, b: 2 }), '{ a: 1, b: 2 }');
+    Object.prototype.hasOwnProperty = has;
+});
