6905cc2f7df35600177e613b0642b4df5efd3eca
James Halliday <mail@substack.net>
2014-08-05 13:23:17 +1200

    more element coverage

--- test/element.js
+++ test/element.js
@@ -14,3 +14,29 @@ test('element', function (t) {
     var obj = [ 1, elem, 3 ];
     t.deepEqual(inspect(obj), '[ 1, <div class="row"></div>, 3 ]');
 });
+    
+test('element no attr', function (t) {
+    t.plan(1);
+    var elem = {
+        nodeName: 'div',
+        getAttribute: function (key) {
+            return elem.attributes[key];
+        },
+        childNodes: []
+    };
+    var obj = [ 1, elem, 3 ];
+    t.deepEqual(inspect(obj), '[ 1, <div></div>, 3 ]');
+});
+
+test('element with contents', function (t) {
+    t.plan(1);
+    var elem = {
+        nodeName: 'div',
+        getAttribute: function (key) {
+            return elem.attributes[key];
+        },
+        childNodes: [ { nodeName: 'b' } ]
+    };
+    var obj = [ 1, elem, 3 ];
+    t.deepEqual(inspect(obj), '[ 1, <div>...</div>, 3 ]');
+});
